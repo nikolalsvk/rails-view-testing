1d132a0913df504b3b166381e0ed0663433978fc
Nikola Đuza <nikolaseap@gmail.com>
2021-03-30 12:54:03 +0200

    Add examples with RSpec and Capybara matchers

--- spec/views/books/index.html.erb_spec.rb
+++ spec/views/books/index.html.erb_spec.rb
@@ -25,4 +25,51 @@ RSpec.describe "books/index", type: :view do
     assert_select "tr>td", text: "Download Url".to_s, count: 2
     assert_select "tr>td", text: "Status".to_s, count: 2
   end
+
+  context "when there are custom books" do
+    before(:each) do
+      assign(:books, [
+        Book.create!(
+          title: "Rails Testing",
+          description: "How to test Ruby on Rails applications.",
+          download_url: nil,
+          status: "draft"
+        ),
+        Book.create!(
+          title: "Rails Patterns",
+          description: "A book about patterns and anti-patterns in Ruby on Rails.",
+          download_url: "rails-patterns.com/download",
+          status: "published"
+        )
+      ])
+    end
+
+    it "renders a list of books with RSpec matchers" do
+      render
+
+      expect(rendered).to match(/Rails Testing/)
+      expect(rendered).to include("Rails Patterns")
+
+      expect(rendered).to match(/How to test Ruby on Rails applications./)
+      expect(rendered).to include("A book about patterns and anti-patterns in Ruby on Rails.")
+
+      expect(rendered).to include("rails-patterns.com/download")
+
+      expect(rendered).to include("published")
+    end
+
+    it "renders a list of books with Capybara matchers" do
+      render
+
+      expect(rendered).to have_selector("tr>td", text: "Rails Testing")
+      expect(rendered).to have_selector("tr>td", text: "Rails Patterns")
+
+      expect(rendered).to have_selector("tr>td", text: "How to test Ruby on Rails applications")
+      expect(rendered).to have_selector("tr>td", text: "A book about patterns and anti-patterns in Ruby on Rails.")
+
+      expect(rendered).to have_selector("tr>td", text: "rails-patterns.com/download")
+
+      expect(rendered).to have_selector("tr>td", text: "published")
+    end
+  end
 end
